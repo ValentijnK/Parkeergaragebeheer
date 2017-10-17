1844fad7cefa2697d8170be2edb0a4927be416d6
Valentijn <valentijnkilian@gmail.com>
2017-10-17 19:54:43 +0200

    License plate sidecode check

--- src/main.py
+++ src/main.py
@@ -18,18 +18,30 @@ def getLicensePlate(photoPath):
     file = {'image': ('image', open('license_plates/' + photoPath, 'rb'))}
     r = requests.post(alprUrl, files=file)
     data = r.json()
-    # print(data)
     license = data['results'][0]['plate']
-    # print(license)
     if(license):
-        # check if license matches sidecode 7
-        if(re.match('\d\d[A-Z][A-Z][A-Z]\d', license)):
+        # check if license matches sidecode 9
+        if re.match('[A-Z][A-Z]\d\d\d[A-Z]', license):
             licenseList = list(license)
             licenseList.insert(2,'-')
             licenseList.insert(6,'-')
             updatedLicense = ''.join(licenseList)
             return (updatedLicense)
-        # if no sidecode 7 is detected fallback to sidecode 6
+        # license plate is not sidecode 9? check if license matches sidecode 8
+        elif re.match('\d[A-Z][A-Z][A-Z]\d\d', license):
+            licenseList = list(license)
+            licenseList.insert(1, '-')
+            licenseList.insert(5, '-')
+            updatedLicense = ''.join(licenseList)
+            return (updatedLicense)
+        # license plate is not sidecode 9 or 8? check if license matches sidecode 7
+        elif re.match('\d\d[A-Z][A-Z][A-Z]\d', license):
+            licenseList = list(license)
+            licenseList.insert(2, '-')
+            licenseList.insert(6, '-')
+            updatedLicense = ''.join(licenseList)
+            return updatedLicense
+        # if the plate does not match one of the above fallback to sidecode 4/5/6
         else:
             licenseList = list(license)
             licenseList.insert(2, '-')
@@ -42,4 +54,4 @@ def getVehicleInfo(license):
     r = requests.get(rdwUrl + license, headers=rdwHeaders)
     print(r.json())
 
-getVehicleInfo(getLicensePlate('kenteken_2.jpg'))
+getVehicleInfo(getLicensePlate('kenteken.png'))
