7b56bff5f337b37b8fe306baa0366a194de25ce8
Valentijn <valentijnkilian@gmail.com>
2017-11-09 04:40:14 +0100

    style(main): code refactor

--- src/main.py
+++ src/main.py
@@ -8,68 +8,71 @@ import base64
 import os
 
 # API keys
-rdwAPI = 'bd91216bb4b6879946c210cdf9dbdfdb00fc75e031816a3c1d89be31ba3512fc'
-alprAPI = 'sk_25f871e3d65e30b86d749fe3'
+rdw_api = 'bd91216bb4b6879946c210cdf9dbdfdb00fc75e031816a3c1d89be31ba3512fc'
+alpr_api = 'sk_25f871e3d65e30b86d749fe3'
 
 # Url endpoints
-rdwUrl = 'https://overheid.io/api/voertuiggegevens/'
-alprUrl = 'https://api.openalpr.com/v2/recognize?secret_key=' + alprAPI + '&recognize_vehicle=0&country=eu&state=nl&return_image=0&topn=10'
+rdw_url = 'https://overheid.io/api/voertuiggegevens/'
+alpr_url = 'https://api.openalpr.com/v2/recognize?secret_key=' + alpr_api + '&recognize_vehicle=0&country=eu&state=nl&return_image=0&topn=10'
 
 # Headers
-rdwHeaders = {'ovio-api-key': rdwAPI}
+rdw_headers = {'ovio-api-key': rdw_api}
+
 
 # API call to get license plate
-def getLicensePlate(photoPath):
-    file = {'image': ('image', open('license_plates/' + photoPath, 'rb'))}
-    r = requests.post(alprUrl, files=file)
+def get_license_plate(photo_path):
+    file = {'image': ('image', open('license_plates/' + photo_path, 'rb'))}
+    r = requests.post(alpr_url, files=file)
     data = r.json()
     license = data['results'][0]['plate']
-    if(license):
+    if (license):
         # check if license matches sidecode 9
         if re.match('[A-Z][A-Z]\d\d\d[A-Z]', license):
-            licenseList = list(license)
-            licenseList.insert(2,'-')
-            licenseList.insert(6,'-')
-            updatedLicense = ''.join(licenseList)
-            return (updatedLicense)
+            license_list = list(license)
+            license_list.insert(2, '-')
+            license_list.insert(6, '-')
+            updated_license = ''.join(license_list)
+            return (updated_license)
         # license plate is not sidecode 9? check if license matches sidecode 8
         elif re.match('\d[A-Z][A-Z][A-Z]\d\d', license):
-            licenseList = list(license)
-            licenseList.insert(1, '-')
-            licenseList.insert(5, '-')
-            updatedLicense = ''.join(licenseList)
-            return (updatedLicense)
+            license_list = list(license)
+            license_list.insert(1, '-')
+            license_list.insert(5, '-')
+            updated_license = ''.join(license_list)
+            return (updated_license)
         # license plate is not sidecode 9 or 8? check if license matches sidecode 7
         elif re.match('\d\d[A-Z][A-Z][A-Z]\d', license):
-            licenseList = list(license)
-            licenseList.insert(2, '-')
-            licenseList.insert(6, '-')
-            updatedLicense = ''.join(licenseList)
-            return updatedLicense
+            license_list = list(license)
+            license_list.insert(2, '-')
+            license_list.insert(6, '-')
+            updated_license = ''.join(license_list)
+            return updated_license
         # if the plate does not match one of the above fallback to sidecode 4/5/6
         else:
-            licenseList = list(license)
-            licenseList.insert(2, '-')
-            licenseList.insert(5, '-')
-            updatedLicense = ''.join(licenseList)
-            return updatedLicense
+            license_list = list(license)
+            license_list.insert(2, '-')
+            license_list.insert(5, '-')
+            updated_license = ''.join(license_list)
+            return updated_license
 
 
-def getVehicleInfo(license):
-    r = requests.get(rdwUrl + license, headers=rdwHeaders)
+def get_vehicle_info(license):
+    r = requests.get(rdw_url + license, headers=rdw_headers)
     return r.json()
 
-def acceptRequest(vehicle, license):
+
+def accept_request(vehicle, license):
     fuel = vehicle['hoofdbrandstof']
     if fuel == 'Benzine':
         check_in(license)
     else:
         max_date = datetime.strptime('2001-01-01', '%Y-%m-%d')
-        date_afgiteDatum = datetime.strptime(str(vehicle['datumeersteafgiftenederland'])[:10], '%Y-%m-%d')
-        if date_afgiteDatum > max_date:
+        issue_date = datetime.strptime(str(vehicle['datumeersteafgiftenederland'])[:10], '%Y-%m-%d')
+        if issue_date > max_date:
             check_in(license)
         else:
             print('Sorry, maar deze vervuilende diesel mag er niet.')
+
 
 def check_in(license):
     connection = pymysql.connect(host='localhost',
@@ -84,6 +87,7 @@ def check_in(license):
             connection.commit()
     finally:
         connection.close()
+
 
 def check_out(license):
     encrypted_license = encrypt_info(license)
@@ -117,20 +121,22 @@ def check_if_exist(license):
     finally:
         connection.close()
 
+
 def encrypt_info(info):
     # A block size of 16 equals 128 bits
-    BLOCK_SIZE = 16
-    PADDING = '{'
-    pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
+    block_size = 16
+    padding = '{'
+    pad = lambda s: s + (block_size - len(s) % block_size) * padding
     # Encrypt with AES, encode with base64
-    EncodeAES = lambda c, s:base64.b64encode(c.encrypt(pad(s)))
+    Encode_AES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
     # Generate a randomized secret key
     secret = b"H\x04 \x9e\x10\x16\xf8\xba\x0b2\xf8\xef}'\x90u"
     # Generate the cipher object using the key
     cipher = AES.new(secret)
     # encode the license plate
-    encoded = EncodeAES(cipher, info)
+    encoded = Encode_AES(cipher, info)
     return encoded
+
 
 # Extra Assignment
 def billing(license):
@@ -155,25 +161,26 @@ def billing(license):
     email = input('Vul hier uw e-mailadres in:')
 
     billing_connection = pymysql.connect(host='localhost',
-                                 user='root',
-                                 password='',
-                                 db='parkeergarage',
-                                 cursorclass=pymysql.cursors.DictCursor)
+                                         user='root',
+                                         password='',
+                                         db='parkeergarage',
+                                         cursorclass=pymysql.cursors.DictCursor)
     try:
         with billing_connection.cursor() as cursor:
             sql = "UPDATE `cars` SET `garage_entry_time` = garage_entry_time, `garage_leave_time` = garage_leave_time, `email` = %s WHERE `license_plate` = %s"
-            cursor.execute(sql, (encrypt_info(email),encrypted_license))
+            cursor.execute(sql, (encrypt_info(email), encrypted_license))
             billing_connection.commit()
     finally:
         billing_connection.close()
 
     print('Er wordt een factuur verzonden naar ' + str(email))
 
-license = getLicensePlate('kenteken_3.jpg')
 
-if(check_if_exist(license) is None):
-    vehicle = getVehicleInfo(license)
-    acceptRequest(vehicle, license)
+license = get_license_plate('kenteken.png')
+
+if (check_if_exist(license) is None):
+    vehicle = get_vehicle_info(license)
+    accept_request(vehicle, license)
 else:
     check_out(license)
     billing(license)
